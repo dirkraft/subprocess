b8ea3f2d0ecb2a5a87cc23633088e0db6e4e19f5
Jason Dunkelberger (aka dirkraft) <dirkraft@gmail.com>
2016-08-02 20:30:38 -0700

    erps threads

--- subprocess/src/test/java/com/github/dirkraft/subprocess/Assumptions.java
+++ subprocess/src/test/java/com/github/dirkraft/subprocess/Assumptions.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertFalse;
@@ -46,14 +47,14 @@ public class Assumptions {
 
 		ExecutorService executorService = Executors.newCachedThreadPool();
 
-		final Holder<String> stdoutHolder = new Holder<>();
+		final AtomicReference<String> stdoutHolder = new AtomicReference<>();
 		executorService.submit(() -> {
-			stdoutHolder.value = Except.log(() -> IOUtils.toString(p.getInputStream(), UTF_8));
+			stdoutHolder.set(Except.log(() -> IOUtils.toString(p.getInputStream(), UTF_8)));
 		});
 
-		final Holder<String> stderrHolder = new Holder<>();
+		final AtomicReference<String> stderrHolder = new AtomicReference<>();
 		executorService.submit(() -> {
-			stderrHolder.value = Except.log(() -> IOUtils.toString(p.getErrorStream(), UTF_8));
+			stderrHolder.set(Except.log(() -> IOUtils.toString(p.getErrorStream(), UTF_8)));
 		});
 
 		assertFalse("This process should be persistent.", p.waitFor(1, TimeUnit.SECONDS));
@@ -64,9 +65,9 @@ public class Assumptions {
 		assertTrue("Stream threads executor stopped", executorService.awaitTermination(1, TimeUnit.SECONDS));
 
 		assertTrue("This output happened before p.destroy(), so it comes through fine.",
-			stdoutHolder.value.contains("Hello"));
+			stdoutHolder.get().contains("Hello"));
 		assertFalse("This output happens after p.destroy(), so the streams are closed before it is read.",
-			stderrHolder.value.contains("SIGTERM"));
+			stderrHolder.get().contains("SIGTERM"));
 	}
 
 }
